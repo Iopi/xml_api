de93758147503e0b13aa3c1167c6aa6718865a07
iopi <hadhim@seznam.cz>
2023-07-24 13:11:17 +0200

    clear unused methods

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-import zipfile
-import xml.etree.ElementTree as ET
 import urllib.request
+import xml.etree.ElementTree as ET
+import zipfile
+
 from flask import Flask, render_template
 
 import constants as c
@@ -79,101 +80,6 @@ def product_names():
     return render_template('names.html', product_names=product_names, page='product_names')
 
 
-def find_items_recursive(element, parent_categories=[], results=[]):
-    """
-    It recursively searches the XML tree for items.
-
-    The function creates a dictionary containing items names and a list of parent category names.
-    The results are gradually added to the 'results' list.
-
-    :param element: XML tree element
-    :param parent_categories: List of parent categories
-    :param results: List of results
-    :return:
-    """
-    if element.find("item") is not None:
-        parent_categories = parent_categories + [element]
-        item_names = [item.attrib.get("name") for item in element.findall(".//item")]
-        parent_category_names = [category.attrib.get("name") for category in parent_categories]
-
-        result = {
-            "items": item_names,
-            "parents": parent_category_names[1:]
-        }
-        results.append(result)
-
-    else:
-        for child in element.findall("category"):
-            find_items_recursive(child, parent_categories + [element], results)
-
-
-def find_categories_recursive(element, parent_categories=[], results=[]):
-    """
-    It recursively searches the XML tree for categories with spare parts.
-
-    It then searches for items in this category using the 'find_items_recursive' function.
-
-    :param element: XML tree element
-    :param parent_categories: List of parent categories
-    :param results: List of results
-    :return:
-    """
-    if element.attrib.get("type") == "part":
-        find_items_recursive(element, parent_categories, results)
-
-    for child in element.findall("category"):
-        find_categories_recursive(child, parent_categories + [element], results)
-
-
-def find_items_recursive_parts(element, parent_categories, results):
-    """
-    It recursively searches the XML tree for items in element 'part'.
-
-    The function creates a dictionary containing items names and a list of parent category names.
-    The results are gradually added to the 'results' list.
-
-    :param element: XML tree element
-    :param parent_categories: List of parent categories
-    :param results: List of results
-    :return:
-    """
-    if element.find("part") is not None:
-        for part in element.findall("part"):
-            if part.find("item") is not None:
-                parent_categories_new = parent_categories + [element]
-                item_names = [item.attrib.get("name") for item in element.findall(".//item")]
-                parent_category_names = [category.attrib.get("name") for category in parent_categories_new] + [
-                    part.attrib.get("itemName")]
-
-                result = {
-                    "items": item_names,
-                    "parents": parent_category_names[1:]
-                }
-                results.append(result)
-
-    else:
-        for child in element.findall("category"):
-            find_items_recursive_parts(child, parent_categories + [element], results)
-
-
-def find_categories_recursive_parts(element, parent_categories=[], results=[]):
-    """
-    It recursively searches the XML tree for categories with spare parts.
-
-    It then searches for items in this category using the 'find_categories_recursive_parts' function.
-
-    :param element: XML tree element
-    :param parent_categories: List of parent categories
-    :param results: List of results
-    :return:
-    """
-    if element.attrib.get("type") == "parts":
-        find_items_recursive_parts(element, parent_categories, results)
-
-    for child in element.findall("category"):
-        find_categories_recursive_parts(child, parent_categories + [element], results)
-
-
 @app.route('/products/spare_parts')
 def product_spare_parts():
     """
@@ -209,9 +115,6 @@ def product_spare_parts():
                 "item_parts": item_parts
             }
             results.append(result)
-
-    #     categories_with_parts = root.find(".//categoriesWithParts")
-    # find_categories_recursive_parts(categories_with_parts, [], results)
 
     return render_template('spare_parts.html', results=results, page='product_spare_parts')
 
